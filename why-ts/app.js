2f186958ec84c84a900c598419e39fc4decb62a2
sinhu <tlsgn26@naver.com>
2024-06-12 21:05:46 +0900

    타입스크립트를 왜 써야 하는지1

--- why-ts/app.js
+++ why-ts/app.js
@@ -1,5 +1,5 @@
 // api url
-var url = 'https://jsonplaceholder.typicode.com/users/1';
+var url = 'https://jsonplaceholder.typicode.com/users/';
 
 // dom
 var username = document.querySelector('#username');
@@ -9,13 +9,35 @@ var address = document.querySelector('#address');
 // user data
 var user = {};
 
+/**
+ * @typedef {object} Address
+ * @property {string} street
+ * @property {string} city
+ */
+
+/**
+ * @typedef {object} User
+ * @property {string} name
+ * @property {string} email
+ * @property {object} address
+ */
+
+/**
+ * @returns {Promise<User>}
+ */
+function fetchUser() {
+  return axios.get(url);
+}
+
 function startApp() {
-  axios
-    .get(url)
+  fetchUser()
     .then(function (response) {
       console.log(response);
       user = response.data;
-      // TODO: 이름, 이메일, 주소 표시하기
+      username.innerText = user[0].name;
+      email.innerText = user[0].email;
+      //undefind 가 발생해서 에러가 발생할만한 부분을 타입스크립트를 이용하면 방지 할 수 있다.
+      address.innerText = user[0].address.street;
     })
     .catch(function (error) {
       console.log(error);
