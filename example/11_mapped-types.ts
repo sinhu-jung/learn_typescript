ca0e046424c008c39cab9fbe476e983a8625680b
sinhu-jung <tlsgn26@naver.com>
2024-09-23 16:10:17 +0900

    mapped type

--- example/11_mapped-types.ts
+++ example/11_mapped-types.ts
@@ -1,4 +1,7 @@
 // mapped types
+// 기존에 정의되어 있는 타입을 새로운 타입으로 변환해 주는 문법
+// 자바스크립트의 map 함수를 타입에 적용한 것과 같은 효과
+
 type Heroes = 'Hulk' | 'Thor' | 'Capt';
 type HeroAges = { [K in Heroes]: number };
 const ages: HeroAges = {
